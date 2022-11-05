fb6a779abb7a38953497107b843176a6fe36771a
SJW <58480379+seeker53@users.noreply.github.com>
2022-11-05 15:00:45 +0530

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,8 +4,8 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "YujiroSMP.aternos.me",
+    port: "29356",
     username: "Test",
     version: false
     })
